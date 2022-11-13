55d7da750e02089ec975d72128074abb2a10ac75
Arno Potgieter <arnopotgieter11@gmail.com>
2022-11-13 15:58:08 +0200

    Migrations

---
+++ StockMeister/Migrations/20221113134403_SettingUp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockMeister.Migrations
+{
+    public partial class SettingUp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
